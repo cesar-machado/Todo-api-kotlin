5a4dd1d7416bd7b79059842d3432b451bfe548ec
César Machado <cesarmach19@gmail.com>
2024-02-12 16:34:43 -0300

    adicionando crossOrigin para conseguir liberar para o frontend consumir.

--- src/main/kotlin/com/cmach/todoapikotlin/controllers/HelloController.kt
+++ src/main/kotlin/com/cmach/todoapikotlin/controllers/HelloController.kt
@@ -1,12 +1,14 @@
 package com.cmach.todoapikotlin.controllers
 
 import com.cmach.todoapikotlin.model.Hello
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import java.util.concurrent.atomic.AtomicLong
 
 @RestController
+@CrossOrigin(origins = ["http://localhost:5173/"])
 class HelloController {
 
     @RequestMapping("/hello")
